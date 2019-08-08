de4eb8aa7b875755abdb532a826dd4d80b93bec1
Micah Levine <micah.levine@outlook.com>
2019-08-08 15:46:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ def speak_to_grandma(phrase)
    elsif phrase == phrase.upcase
      return "HUH?! SPEAK UP, SONNY!".upcase
    else
-     return "NO, NOT SINCE 1938!"
+     return "HUH?! SPEAK UP, SONNY!".upcase
   end
 end
 
